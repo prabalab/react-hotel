044c47ba5c8adc99b5f2774cca677551646a1c47
prabalab <40490211+prabalab@users.noreply.github.com>
2025-02-02 14:46:43 +0530

    Update homeRoutes.js

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -3,13 +3,27 @@ const Home = require("../models/homeModel"); // Import the model
 
 const router = express.Router();
 
-// API Endpoint to Fetch Home Data
+// Fetch Home Data
 router.get("/", async (req, res) => {
   try {
     const homeData = await Home.findOne();
     res.json(homeData);
   } catch (error) {
     res.status(500).json({ error: "Error fetching home data" });
+  }
+});
+
+// Insert Home Data
+router.post("/", async (req, res) => {
+  try {
+    const { title, description } = req.body;
+
+    const newHome = new Home({ title, description });
+    await newHome.save();
+
+    res.status(201).json({ message: "Home data inserted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Error inserting home data" });
   }
 });
 
